7152511079fba250862581bd7ae1d1ac29ee83aa
Pedro Henrique <phandradesa@gmail.com>
2023-09-17 17:43:26 -0300

    ✨task-008 - realizado  a alteração no arquivo app.js

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+
+import Login from './src/components/Login';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './src/components/Home';
+import Pokemon from './src/components/Pokemon';
+
+const Stack = createNativeStackNavigator()
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Olá mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName='Login'>
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Pokemon" component={Pokemon} />
+      
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
+    
+  
+   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
