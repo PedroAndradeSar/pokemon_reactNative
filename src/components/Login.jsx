a13e1b5c60ef67183a1520f2ea1a474f86bce345
Pedro Henrique <phandradesa@gmail.com>
2023-09-17 17:14:28 -0300

    🚧 task-005 - finalizada a tela de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Image, StyleSheet, View } from "react-native";
 import { TextInput } from "react-native";
 import MyButton from "./MyButton";
 
 const usuariosEstaticos = {
-{    email: 'phandradesar@gmail.com',
-    senha: '123123',},
-    {    email: 'phandradesar@gmail.com',
-    senha: '123123',}
+    email: 'phandradesar@gmail.com',
+    senha: '123123',
 };
 
 const Login = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
+
+    //rubinho, fiz essas mudanças
+    const [isValidEmail, setIsValidEmail] = useState(false);
+
+    useEffect(() => {
+        setIsValidEmail(validateEmail(email));
+    }, [email]);
+
+    const validateEmail = (text) => {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(text) && text.length >= 5; // Aqui tem que ter mínimo de 5 caracteres para o email
+    };
+
+    const handleLogin = () => {
+        if (isValidEmail && senha.length >= 6) {
+            if (email === usuariosEstaticos.email && senha === usuariosEstaticos.senha) {
+                // Aqui faz Login bem-sucedido, navegue para a página 'Home'
+                navigation.navigate('Pokemon');
+
+                setEmail('');
+                setSenha('');
+            } else {
+                // Aqui exibe uma mensagem de erro, pois os dados estão incorretos
+                alert('Email ou senha incorretos');
+            }
+        } else {
+            // Exibe uma mensagem de erro se os campos não atenderem aos critérios
+            alert('Email ou senha incorretos');
+        }
+    };
+
 
     return (
         <View style={styles.container}>
@@ -26,19 +55,27 @@ const Login = ({ navigation }) => {
             <View style={styles.Input}>
                 <TextInput
                     placeholderTextColor='red'
+                    style={[
+                        styles.textImput,
+                        email.length >= 5 && !isValidEmail ? { borderColor: 'red' } : null,
+                    ]}
                     placeholder={"Email (mínimo de 5 caracteres)"}
                     onChangeText={(text) => setEmail(text)}
                     value={email}
                 />
                 <TextInput
                     placeholderTextColor='red'
-                    placeholder={"Senha"}
+                    style={[
+                        styles.textImput,
+                        senha.length < 6 ? { borderColor: 'red' } : null,
+                    ]}
+                    placeholder={"Senha (mínimo de 6 caracteres)"}
                     secureTextEntry={true}
                     onChangeText={(text) => setSenha(text)}
                     value={senha}
                 />
             </View>
-            <MyButton title='ENTRAR'/>
+            <MyButton title='ENTRAR' onPressButton={handleLogin} disabled={!isValidEmail || senha.length < 6} />
         </View>
     );
 };
@@ -53,9 +90,22 @@ const styles = StyleSheet.create({
         backgroundColor: '#e8d209',
     },
     textImput: {
+        width: 250,
+        borderWidth: 2,
+        borderRadius: 10,
+        padding: 10,
+        margin: 15,
+        borderColor: 'black',
+        backgroundColor: 'white',
+        textAlign: "center",
+        placeHolderTextColor: 'white',
     },
     image: {
+        height: 210,
+        width: 200,
+        marginTop: -100,
     },
     Input: {
+        margin: 40,
     },
 });
