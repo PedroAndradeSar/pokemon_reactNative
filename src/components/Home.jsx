9fc88c46697531c00352394394b9e26278e4be34
Pedro Henrique <phandradesa@gmail.com>
2023-09-17 20:37:51 -0300

    task-009 - aumentando a quantidade de informaçoes e inicianlizando o style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,20 +3,36 @@ import { View, Text, Image } from "react-native";
 
 const Home = ({ route }) => {
   const { pokemonData } = route.params;
-  const { name, types, sprites } = pokemonData;
+  const { name, types, sprites, height, weight, abilities } = pokemonData;
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.container}>
       <Image
         source={{ uri: sprites.front_default }}
-        style={{ width: 200, height: 200 }}
+        style={styles.image}
       />
-      <Text>Name: {name}</Text>
-      <Text>
-        Type: {types.map((type) => type.type.name).join(", ")}
-      </Text>
+      <Text style={styles.text}>Name: {name}</Text>
+      <Text style={styles.text}>Type: {types.map((type) => type.type.name).join(", ")}</Text>
+      <Text style={styles.text}>Height: {height}</Text>
+      <Text style={styles.text}>Weight: {weight}</Text>
+      <Text style={styles.text}>Abilities: {abilities.map((ability) => ability.ability.name).join(", ")}</Text>
     </View>
   );
 };
+  const styles = {
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    image: {
+      width: 200,
+      height: 200,
+    },
+    text: {
+      fontSize: 16,
+      marginVertical: 5,
+    },
+  };
 
 export default Home;
