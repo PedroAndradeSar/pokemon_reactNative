b7c13f513d92e4e0ca97556f8559b6bd86de05fe
Pedro Henrique <phandradesa@gmail.com>
2023-09-17 19:46:51 -0300

    task-009 - funcionando a os detalhes basicos e a imagem, foi preciso alterar o cod pokemon.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
-const Home = () => {
-    return (
-        <Text>Ola mundo</Text>
-    );
-}
+const Home = ({ route }) => {
+  const { pokemonData } = route.params;
+  const { name, types, sprites } = pokemonData;
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Image
+        source={{ uri: sprites.front_default }}
+        style={{ width: 200, height: 200 }}
+      />
+      <Text>Name: {name}</Text>
+      <Text>
+        Type: {types.map((type) => type.type.name).join(", ")}
+      </Text>
+    </View>
+  );
+};
+
 export default Home;
