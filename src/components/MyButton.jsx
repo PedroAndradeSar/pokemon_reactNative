7f2da897320ac02b1d726d5004094b66e50461d8
rjunior2209 <robsonjunior.gonsalves@gmail.com>
2023-09-17 12:49:26 -0300

    task-006 -criação e style do MyButton

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,10 +1,32 @@
-import { Text } from "react-native";
-import React from "react";
+import { StyleSheet, Text ,TouchableOpacity, View } from "react-native";
 
-const MyButton = () => {
+
+
+const MyButton = ({title,onPressButton}) => {
     return (
-        <Text>KJDUYQ</Text>
+        <TouchableOpacity onPress={onPressButton}>
+            <View style={styles.buttonContainer}>
+                <Text style={styles.buttonTitle}>{title}</Text>
+            </View>
+        </TouchableOpacity>
+        
     );
 }
 
 export default MyButton;
+const styles = StyleSheet.create({
+    buttonContainer:{
+        shadowRadius:5,
+        shadowOpacity:0.8,
+        shadowColor:'#212121',
+        padding:8,
+        borderRadius:10,
+        backgroundColor:'blue',
+        margin:4
+    },
+            buttonTitle:{
+                color:'#fff',
+                fontSize:18
+
+            }
+})
