1a84c34a77c54f0c459d9c45bca9d5aa7b7e4815
Pedro Henrique <phandradesa@gmail.com>
2023-09-17 15:37:33 -0300

    task-005 - teste de mais logins

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,24 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
+=======
+import React, { useState } from "react";
+>>>>>>> 59460f7 (task-005)
 import { Image, StyleSheet, View } from "react-native";
 import { TextInput } from "react-native";
 import MyButton from "./MyButton";
 
 const usuariosEstaticos = {
-    email: 'phandradesar@gmail.com',
-    senha: '123123',
+{    email: 'phandradesar@gmail.com',
+    senha: '123123',},
+    {    email: 'phandradesar@gmail.com',
+    senha: '123123',}
 };
 
+<<<<<<< HEAD
 const Login = () => {
+=======
+const Login = ({ navigation }) => {
+>>>>>>> 59460f7 (task-005)
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
 
@@ -24,7 +34,11 @@ const Login = () => {
             <View style={styles.Input}>
                 <TextInput
                     placeholderTextColor='red'
+<<<<<<< HEAD
                     placeholder={"Email"}
+=======
+                    placeholder={"Email (mínimo de 5 caracteres)"}
+>>>>>>> 59460f7 (task-005)
                     onChangeText={(text) => setEmail(text)}
                     value={email}
                 />
@@ -45,6 +59,13 @@ export default Login;
 
 const styles = StyleSheet.create({
     container: {
+<<<<<<< HEAD
+=======
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+        backgroundColor: '#e8d209',
+>>>>>>> 59460f7 (task-005)
     },
     textImput: {
     },
